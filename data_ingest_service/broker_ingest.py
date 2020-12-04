8d20b514c73024a191f103de81a5886444a8093c
Preston Turner <preston.turner@adapt2solutions.com>
2020-12-04 10:58:18 -0600

    changes to ingest

--- data_ingest_service/broker_ingest.py
+++ data_ingest_service/broker_ingest.py
@@ -1,25 +1,32 @@
 
 #import context
-import paho.mqtt.subscribe as subscribe
+import paho.mqtt.client as mqtt
 import json
 import time
 import random
+import uuid
 
-
-
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
 
 
 def main():
+    client_name  = f"starfox:{uuid.uuid4()}"
+    client = mqtt.Client(client_name)
     while True:
-        try:
-            m = subscribe.simple("/apt326/#", hostname="mqtt-broker",retained=False)
-            print('Message from: {} '.format(m.topic))
-            print('Data: {} '.format(m.payload))
-        except:
-            print(f"Not able to connect to broker at this time...")
+
+        client.on_message = on_message
+        client.connect(host="base_station_mqtt-broker_1",port=1883,keepalive=60)
+        client.loop_start()
+
+        client.subscribe("cot/research")
+        time.sleep(2)
 
 
 
-
-if __name__=='__main__':
-    main()
+            
+if __name__ == '__main__':
+        main()
