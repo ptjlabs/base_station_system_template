009a079785d34c432e940635c0371c3dac3cc60f
Preston Turner <preston.turner@adapt2solutions.com>
2020-12-11 08:51:01 -0600

    main changes

--- data_store_service/app.py
+++ data_store_service/app.py
@@ -9,20 +9,14 @@ import os
 
 
 app = Flask(__name__)
-parser = ConfigParser()
-password = ""
-username = ""
-mongo_uri = ""
-db_name = ""
 
-#configPath = r'C:\Users\preston.turner\Desktop\base_station\data_store_service\dev.ini'
-configPath = r'.\data_store_service\dev.ini'
-parser.read(configPath)
+
+
     
-password = parser.get('MONGO','password')
-username = parser.get('MONGO','username')
-mongo_uri = parser.get('MONGO','mongouri')
-db_name = parser.get('MONGO','dbname')
+password = 'yLYfckRV0zleZm7Z'
+username = 'prezlo'
+mongo_uri = 'cougar-data-den-otljj.mongodb.net/campusDB?retryWrites=true&w=majority'
+db_name = 'campusDB'
 
 
 
@@ -39,21 +33,35 @@ def home():
     return "Living Campus API v2 "
 
 # This route allows iOS applications to post its form content to MongoDB Atlas
-@app.route('/api/v1/submit', methods=['POST'])
+@app.route('/api/v2/submit', methods=['POST'])
 def send_to_db():
     request_data = request.get_json()
 
     person_name = 'not-available'
-    if request_data['name'] != None or request_data['name'] != "":
-        person_name = request_data['name']
-    packet = {
-        # 10/22/2019 - Updated 
-        'temp-rating' : request_data['temp-rating'],
-        'name': person_name
+    res = not bool(request_data)
+    if res:
+        packet = {
+            # 10/22/2019 - Updated 
+            'temp-rating' : request_data['temp-rating'],
+            'name': request_data['name']
+            }
+    cougar_profiles = mongo.db['cougar_profiles']
+    cougar_profiles.insert_one(packet)
+    return jsonify({'message':'success.'}), 200
+
+@app.route('/api/v2/ingest', methods=['POST'])
+def ingest_data():
+    request_data = request.get_json()
+    hasData = not bool(request_data)
+    if hasData:
+        packet = {
+            'temp-rating' : request_data['topic'],
+            'name' : request_data['data']
         }
     cougar_profiles = mongo.db['cougar_profiles']
     cougar_profiles.insert_one(packet)
     return jsonify({'message':'success.'}), 200
+
 
     
 # when a user hits this endpoint the server will respond with available data in the mongoDB database
--- data_store_service/config_file_read.py
+++
@@ -1,7 +0,0 @@
-from configparser import ConfigParser
-
-parser = ConfigParser()
-parser.read(r'C:\Users\preston.turner\Desktop\base_station\data_store_service\dev.ini')
-
-print(parser.sections())
-print(parser.get('MONGO','username'))
